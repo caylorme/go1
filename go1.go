2229635b2c0470790c6b37a6b4b831feb1a199ec
jason <jason@sl-jason.(none)>
2014-04-23 04:25:25 -0400

    Test

--- go1.go
+++ go1.go
@@ -1,9 +1,38 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"net/http"
+	"strings"
 )
 
+func defaultHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Default:  %s", r.URL.Path)
+}
+func APIHandler(w http.ResponseWriter, r *http.Request) {
+	Test := strings.Split(r.URL.Path, "/")
+	switch strings.ToLower(Test[2]) {
+	case "wow":
+		fmt.Fprintf(w, "WOW!")
+	default:
+		Test2 := r.URL.Query()
+		fmt.Printf("Test: %v\n", Test2.Get("test"))
+		js, err := json.Marshal(Test2)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+		}
+
+		w.Header().Set("Content-Type", "application/json")
+		w.Write(js)
+	}
+}
+
 func main() {
-	fmt.Printf("Go!\n")
+	http.HandleFunc("/", defaultHandler)
+	http.HandleFunc("/API/", APIHandler)
+	err := http.ListenAndServe(":7777", nil)
+	if err != nil {
+		fmt.Println("ListenAndServe: ", err)
+	}
 }
